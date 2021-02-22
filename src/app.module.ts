32f1e1e9f8c7e2a95085047d05b115d62100d78f
Filipe Nunes <filipenabrantes@gmail.com>
2021-02-22 17:48:32 -0300

    added redis cache to avoid duplication

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
+import { RedisModule } from 'nestjs-redis';
 import { ManagerModule } from './manager/manager.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -13,6 +14,10 @@ const configService = new ConfigService();
       envFilePath: ['.env'],
     }),
     ScheduleModule.forRoot(),
+    RedisModule.register({
+      url: `redis://${configService.get('REDIS_HOST')}:${configService.get('REDIS_PORT')}?
+        db=${configService.get('REDIS_DB')}&password=${configService.get('REDIS_PASSWORD')}`
+    }),
     MongooseModule.forRoot(configService.get('MONGO_SERVER')),
     ManagerModule,
     SharedModule
